83c51e78d6c74e951375fbf50472cacadb2c4f7c
Kirill777-web <ki.lazutin@gmail.com>
2023-08-31 14:55:14 -0300

    added files

---
+++ Assets/app.js
@@ -0,0 +1,127 @@
+const cityName = document.querySelector('.showweather h2');
+const temperature = document.querySelector('.showweather .temperature span');
+const wind = document.querySelector('.showweather .wind span');
+const humidity = document.querySelector('.showweather .humidity span');
+const currentIconElement = document.querySelector('.showweather .current-icon');
+const forecastCards = document.querySelectorAll('.forecast-card');
+const searchButton = document.querySelector('.btn');
+const searchBox = document.getElementById('searchBox');
+const config = config.apiKey;
+const savedCitiesUl = document.getElementById('saved'); // Reference to saved cities UL
+
+// Load saved cities from local storage on page load
+function loadSavedCities() {
+  const savedCities = JSON.parse(localStorage.getItem('cities')) || [];
+  savedCities.forEach((city) => {
+    appendCityToList(city);
+  });
+}
+
+// Save the city to local storage
+function saveCityToLocalStorage(city) {
+  const savedCities = JSON.parse(localStorage.getItem('cities')) || [];
+  if (!savedCities.includes(city)) {
+    savedCities.push(city);
+    localStorage.setItem('cities', JSON.stringify(savedCities));
+  }
+}
+
+// Append city to the saved list (only if not already in the list)
+function appendCityToList(city) {
+  const existingCities = Array.from(savedCitiesUl.children).map(
+    (li) => li.textContent
+  );
+  if (!existingCities.includes(city)) {
+    const li = document.createElement('li');
+    li.textContent = city;
+    savedCitiesUl.appendChild(li);
+  }
+}
+
+function fetchWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.apiKey}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather data');
+      }
+      return response.json();
+    })
+    .then((data) => displayWeather(data))
+    .catch((error) => console.error('There was an error!', error));
+}
+
+function displayWeather(data) {
+  cityName.textContent = data.name;
+  temperature.textContent = (data.main.temp - 273.15).toFixed(2) + '°C';
+  wind.textContent = data.wind.speed + ' km/h';
+  humidity.textContent = data.main.humidity + '%';
+  const iconCode = data.weather[0].icon;
+  currentIconElement.src = `http://openweathermap.org/img/w/${iconCode}.png`;
+}
+
+function populateForecast(data) {
+  let currentDate = new Date();
+
+  for (let i = 0; i < 5; i++) {
+    const dateElement = forecastCards[i].querySelector('h3');
+    const tempElement = forecastCards[i].querySelector('.temperature span');
+    const windElement = forecastCards[i].querySelector('.wind span');
+    const humidityElement = forecastCards[i].querySelector('.humidity span');
+    const iconElement = forecastCards[i].querySelector('.forecast-icon');
+
+    const forecast = data.list[i * 8];
+    dateElement.textContent = `${currentDate.getDate()}/${
+      currentDate.getMonth() + 1
+    }/${currentDate.getFullYear()}`;
+
+    tempElement.textContent = `${Math.round(forecast.main.temp - 273.15)}°C`;
+    windElement.textContent = `${forecast.wind.speed} km/h`;
+    humidityElement.textContent = `${forecast.main.humidity}%`;
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    const iconCode = forecast.weather[0].icon;
+    iconElement.src = `http://openweathermap.org/img/w/${iconCode}.png`;
+  }
+}
+
+function fetchForecast(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${config.apiKey}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch forecast data');
+      }
+      return response.json();
+    })
+    .then((data) => populateForecast(data))
+    .catch((error) => console.error('There was an error!', error));
+}
+
+//Clicking the search button
+document.querySelector('.btn').addEventListener('click', function () {
+  const city = searchBox.value;
+
+  if (city) {
+    saveCityToLocalStorage(city); // Save the city
+    appendCityToList(city);
+    fetchWeather(city);
+    fetchForecast(city);
+  } else {
+    console.log('Please enter a city name.');
+  }
+});
+
+// Clicking on a saved city to fetch its weather
+savedCitiesUl.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    const city = e.target.textContent;
+    fetchWeather(city);
+    fetchForecast(city);
+  }
+});
+
+// Load cities from local storage upon loading the page
+loadSavedCities();
---
+++ README.md
@@ -0,0 +1,71 @@
+## Weather Whisper
+
+## Description
+
+The Weather App is a user-friendly platform designed to provide current weather details as well as a 5-day forecast for any city in the world. Using the OpenWeatherMap API, it fetches accurate and up-to-date weather conditions. The app not only presents the data visually in a structured manner but also allows users to save their searched cities and revisit the weather details by simply clicking on them.
+
+## Features
+
+- Search Weather by City: Users can input any city globally to retrieve its current weather details.
+- 5-Day Forecast: Alongside the current weather, the app provides a 5-day forecast with information on temperature, wind speed, and humidity.
+- Dynamic Icons: Based on the current weather conditions, the app displays relevant icons.
+- Saved Cities List: Users can view their previously searched cities. This list persists even after the page is refreshed thanks to local storage integration.
+- Click-to-View: The saved cities aren't just a static list; users can click on them to view the weather details for that city instantly.
+- Responsive Design: The app is designed to be used on various screen sizes without compromising user experience.
+
+## Usage
+
+1. Getting Started: Open the Weather App in your preferred browser.
+2. Searching for a City: Simply input the desired city into the search box and click on the search button (or hit enter).
+3. Viewing Weather Details: Once a city is searched, the app will display the current weather conditions at the top, followed by a 5-day forecast. Each day displays temperature, wind speed, and humidity information.
+4. Saved Cities: On searching for a city, it's automatically added to the saved cities list on the left. This allows users to quickly revisit a city's weather by simply clicking on it from the list.
+5. Refreshing & Persistence: Feel free to refresh the app or come back to it later. Your saved cities list will remain intact.
+
+## Installation:
+
+N/A
+
+## ⚠️ Warning: Extension Conflict
+
+- If you're using the Pro Writing Aid extension (particularly in Chrome), you might experience display issues or unexpected behavior with this application. This extension modifies webpage content, which can sometimes conflict with the intended functionality of web apps.
+
+* Recommended Actions:
+
+- If you experience unexpected behavior, try disabling the Pro Writing Aid extension and then reload the page.
+  Alternatively, you can use our application in incognito mode (or a mode where extensions are not loaded) to ensure a smooth experience.
+
+  ## Links
+
+- This is the link of my application [GitHub pages]()
+- This is the link of my [GitHub repository](https://github.com/Kirill777-web/Weather-Whisper)
+
+- The following screenshots show the application final output:
+  ![Weather Whisper app](/Assets/weather_whisper.png)
+
+  ## Credits
+
+- This is link of UNB course section JavaScript/Fetch [UNB](https://courses.bootcampspot.com/courses/3974)
+- This is link of MDN documentation [MDN](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+- OpenWeather API [Weather API](https://openweathermap.org/api)
+
+## License
+
+Copyright (c) 2023 Kirill Lazutin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
